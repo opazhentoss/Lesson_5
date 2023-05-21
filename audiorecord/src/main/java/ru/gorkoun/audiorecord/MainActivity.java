5b297c2bfb266f0dd96f4cf628dbf7003a9e11e9
evgen <gorkoune@gmail.com>
2023-05-21 09:58:41 +0300

    commit

--- audiorecord/src/main/java/ru/gorkoun/audiorecord/MainActivity.java
+++ audiorecord/src/main/java/ru/gorkoun/audiorecord/MainActivity.java
@@ -2,80 +2,133 @@ package ru.gorkoun.audiorecord;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.IOException;
+
 import com.example.audiorecord.databinding.ActivityMainBinding;
 
-import java.io.File;
-
 public class MainActivity extends AppCompatActivity {
+
     private ActivityMainBinding binding;
-    private static final int REQUEST_CODE_PERMISSION = 200;
-    private final String TAG = MainActivity.class.getSimpleName();
-    private boolean isWork;
-    private String fileName = null;
-    private Button recordButton = null;
-    private Button playButton = null;
-    private MediaRecorder recorder = null;
-    private MediaPlayer player = null;
-    boolean isStartRecording = true;
-    boolean isStartPlaying = true;
+    private	static	final	int	REQUEST_CODE_PERMISSION	=	200;
+    private	boolean	isWork;
+    private	final	String	TAG	=	MainActivity.class.getSimpleName();
+    private Button recordButton	=	null;
+    private	Button	playButton	=	null;
+    private MediaRecorder recorder	=	null;
+    private MediaPlayer player	=	null;
+    private String recordFilePath = null;
+    boolean	isStartRecording	=	true;
+    boolean	isStartPlaying	=	true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-// инициализация setContentView()
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-// инициализация кнопок записи и воспроизведения
-        recordButton = binding.recordButton;
-        playButton = binding.playButton;
+
+        recordButton	=	binding.recordButton;
+        playButton	=	binding.playButton;
         playButton.setEnabled(false);
-        recordFilePath = (new File(getExternalFilesDir(Environment.DIRECTORY_MUSIC),
+        recordFilePath	=	(new File(getExternalFilesDir(Environment.DIRECTORY_MUSIC),
                 "/audiorecordtest.3gp")).getAbsolutePath();
-// проверка разрешений на запись аудио и запись на внешнюю память
-        recordButton.setOnClickListener(new View.OnClickListener() {
+
+        int	audioRecordPermissionStatus	=	ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO);
+        int	storagePermissionStatus	=	ContextCompat.checkSelfPermission(this,	android
+                .Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if	(audioRecordPermissionStatus	==	PackageManager.PERMISSION_GRANTED	&&
+                storagePermissionStatus ==	PackageManager.PERMISSION_GRANTED)	{
+            isWork	=	true;
+        }	else	{
+            ActivityCompat.requestPermissions(this,	new	String[]	{android.Manifest
+                            .permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE_PERMISSION);
+        }
+        recordButton.setOnClickListener(new	View.OnClickListener()	{
             @Override
-            public void onClick(View v) {
-                if (mStartRecording) {
-                    recordButton.setText("Stop recording");
+            public	void	onClick(View v)	{
+                if	(isStartRecording)	{
+                    recordButton.setText("Stop	recording");
                     playButton.setEnabled(false);
-                } else {
-                    recordButton.setText("Start recording");
+                    startRecording();
+                }	else	{
+                    recordButton.setText("Start	recording");
                     playButton.setEnabled(true);
+                    stopRecording();
                 }
-                isStartRecording = !isStartRecording;
+                isStartRecording	=	!isStartRecording;
             }
         });
-        playButton.setOnClickListener(new View.OnClickListener() {
+        playButton.setOnClickListener(new	View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mStartPlaying) {
-                    playButton.setText("Stop playing");
+                if (isStartPlaying) {
+                    playButton.setText("Stop	playing");
                     recordButton.setEnabled(false);
+                    startPlaying();
                 } else {
-                    playButton.setText("Start playing");
+                    playButton.setText("Start	playing");
                     recordButton.setEnabled(false);
+                    stopPlaying();
                 }
                 isStartPlaying = !isStartPlaying;
+            }
+        });
+    }
+    private	void startRecording() {
+        recorder	=	new	MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(recordFilePath);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try	{
+            recorder.prepare();
+        }	catch	(IOException e)	{
+            Log.e(TAG,	"prepare()	failed");
         }
+        recorder.start();
+    }
+    private	void	stopRecording()	{
+        recorder.stop();
+        recorder.release();
+        recorder	=	null;
+    }
+    private	void	startPlaying()	{
+        player	=	new	MediaPlayer();
+        try	{
+            player.setDataSource(recordFilePath);
+            player.prepare();
+            player.start();
+        }	catch	(IOException	e)	{
+            Log.e(TAG,	"prepare()	failed");
+        }
+    }
+    private	void	stopPlaying()	{
+        player.release();
+        player	=	null;
     }
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-// производится проверка полученного результата от пользователя на запрос разрешения Camera
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case REQUEST_CODE_PERMISSION:
-                isWork = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+    public	void	onRequestPermissionsResult(int	requestCode, @NonNull String[]	permissions,
+                                                 @NonNull	int[] grantResults)	{
+        super.onRequestPermissionsResult(requestCode,	permissions,	grantResults);
+        switch	(requestCode){
+            case	REQUEST_CODE_PERMISSION:
+                isWork		=	grantResults[0]	==	PackageManager.PERMISSION_GRANTED;
                 break;
         }
-        if (!isWork ) finish();
+        if	(!isWork	)	finish();
     }
-
 }
